cfb90ea151ebefca1ade02c98f9cf664ab35e6c4
marjpw <tapiaurb.jmt@gmail.com>
2025-11-25 23:48:33 -0500

    Delete VitalSoft-main directory

--- VitalSoft-main/src/generarhash.java
+++
@@ -1,9 +0,0 @@
-import org.mindrot.jbcrypt.BCrypt;
-
-public class generarhash {
-    public static void main(String[] args) {
-        String hash = BCrypt.hashpw("1234", BCrypt.gensalt(10));
-        System.out.println(hash);
-    }
-
-}
